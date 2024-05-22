aafa6ce892905405c2907271b098d5e60cae6cc8
David Nanciu <nanciudavid@outlook.com>
2024-05-23 02:25:37 +0300

    Added SidePanel

--- Forms/Form1.Designer.cs
+++ Forms/Form1.Designer.cs
@@ -28,7 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panelMenu = new Panel();
+            panelLogo = new Panel();
+            btnPerson = new Button();
+            btnInterview = new Button();
+            btnAnalysis = new Button();
+            btnDashboard = new Button();
+            panelMenu.SuspendLayout();
             SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.FromArgb(51, 51, 76);
+            panelMenu.Controls.Add(btnDashboard);
+            panelMenu.Controls.Add(btnAnalysis);
+            panelMenu.Controls.Add(btnInterview);
+            panelMenu.Controls.Add(btnPerson);
+            panelMenu.Controls.Add(panelLogo);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(220, 536);
+            panelMenu.TabIndex = 0;
+            // 
+            // panelLogo
+            // 
+            panelLogo.BackColor = Color.FromArgb(39, 39, 58);
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Padding = new Padding(12, 0, 0, 0);
+            panelLogo.Size = new Size(220, 80);
+            panelLogo.TabIndex = 0;
+            // 
+            // btnPerson
+            // 
+            btnPerson.Dock = DockStyle.Top;
+            btnPerson.FlatAppearance.BorderSize = 0;
+            btnPerson.FlatStyle = FlatStyle.Flat;
+            btnPerson.ForeColor = Color.White;
+            btnPerson.Image = Properties.Resources.user__1_;
+            btnPerson.ImageAlign = ContentAlignment.MiddleLeft;
+            btnPerson.Location = new Point(0, 80);
+            btnPerson.Name = "btnPerson";
+            btnPerson.Padding = new Padding(12, 0, 0, 0);
+            btnPerson.Size = new Size(220, 60);
+            btnPerson.TabIndex = 0;
+            btnPerson.Text = "    Person";
+            btnPerson.TextAlign = ContentAlignment.MiddleLeft;
+            btnPerson.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnPerson.UseVisualStyleBackColor = true;
+            // 
+            // btnInterview
+            // 
+            btnInterview.Dock = DockStyle.Top;
+            btnInterview.FlatAppearance.BorderSize = 0;
+            btnInterview.FlatStyle = FlatStyle.Flat;
+            btnInterview.ForeColor = Color.White;
+            btnInterview.Image = (Image)resources.GetObject("btnInterview.Image");
+            btnInterview.ImageAlign = ContentAlignment.MiddleLeft;
+            btnInterview.Location = new Point(0, 140);
+            btnInterview.Name = "btnInterview";
+            btnInterview.Padding = new Padding(12, 0, 0, 0);
+            btnInterview.Size = new Size(220, 60);
+            btnInterview.TabIndex = 1;
+            btnInterview.Text = "    Interview";
+            btnInterview.TextAlign = ContentAlignment.MiddleLeft;
+            btnInterview.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnInterview.UseVisualStyleBackColor = true;
+            // 
+            // btnAnalysis
+            // 
+            btnAnalysis.Dock = DockStyle.Top;
+            btnAnalysis.FlatAppearance.BorderSize = 0;
+            btnAnalysis.FlatStyle = FlatStyle.Flat;
+            btnAnalysis.ForeColor = Color.White;
+            btnAnalysis.Image = (Image)resources.GetObject("btnAnalysis.Image");
+            btnAnalysis.ImageAlign = ContentAlignment.MiddleLeft;
+            btnAnalysis.Location = new Point(0, 200);
+            btnAnalysis.Name = "btnAnalysis";
+            btnAnalysis.Padding = new Padding(12, 0, 0, 0);
+            btnAnalysis.Size = new Size(220, 60);
+            btnAnalysis.TabIndex = 2;
+            btnAnalysis.Text = "    Analysis Results";
+            btnAnalysis.TextAlign = ContentAlignment.MiddleLeft;
+            btnAnalysis.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnAnalysis.UseVisualStyleBackColor = true;
+            // 
+            // btnDashboard
+            // 
+            btnDashboard.Dock = DockStyle.Top;
+            btnDashboard.FlatAppearance.BorderSize = 0;
+            btnDashboard.FlatStyle = FlatStyle.Flat;
+            btnDashboard.ForeColor = Color.White;
+            btnDashboard.Image = (Image)resources.GetObject("btnDashboard.Image");
+            btnDashboard.ImageAlign = ContentAlignment.MiddleLeft;
+            btnDashboard.Location = new Point(0, 260);
+            btnDashboard.Name = "btnDashboard";
+            btnDashboard.Padding = new Padding(12, 0, 0, 0);
+            btnDashboard.Size = new Size(220, 60);
+            btnDashboard.TabIndex = 3;
+            btnDashboard.Text = "    Dashboard";
+            btnDashboard.TextAlign = ContentAlignment.MiddleLeft;
+            btnDashboard.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnDashboard.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -37,12 +141,21 @@
             BackColor = Color.White;
             BackgroundImageLayout = ImageLayout.Center;
             ClientSize = new Size(848, 536);
+            Controls.Add(panelMenu);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            panelMenu.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelMenu;
+        private Panel panelLogo;
+        private Button btnPerson;
+        private Button btnDashboard;
+        private Button btnAnalysis;
+        private Button btnInterview;
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# PROIECTWAP
+# PROIECTWAP# Windows Applications Programming Project
+
+
+Presentation of the Project assigned during the Windows Application Progamming class 2024. 
+Given project topic : **Statistics**
+
+# Requirements
+
+ 
+Declare / implement the entities (classes)
+
+- check the "Designing your classes" section in the document containing the project topics
+
+<bold> Person, Interview, AnalysisResult </bold>
+
+Create forms that allow users to input / update / delete data (3points)
+
+- 1.50p: CRUD for the classes specified in the list of project topics  
+- 1.50p: Well-designed interface (check also the "Designing your interface" section in the document containing the project topics)
+
+Add data validation (ErrorProvider control, Validating/Validated events, standard exceptions, custom exceptions)  (0.5 points) 
+- in at least in one place      
+
+Implement Alt Shortcuts (0.25 points)
+
+- in at least in one place 
+
+Add data serialization / deserialization  (0.25 points)
+
+ - for at least one class 
+
+Add the option to export a report as a txt file (0.25 points)
+
+- in at least in one place 
+
+Use the following menu controls: MenuStrip, ToolStrip, StatusStrip, ContextMenuStrip (0.50 points)
+- at least once each of them  
+- you should use the controls in a meaningful way
+
+Draw a chart using the System.Drawing.Graphics class, in order to represent some statistics that are meaningful for your application (0.50 points)
+
+- You should not use an existing chart control, like the one available in the ToolBox (the goal is to create such a control yourself).  
+- The control should be different from the ones available on http://github.com/liviucotfas (discussed during the course / during the labs).  
+- You can check the charts in Microsoft Excel for inspiration.
+
+Use a relational database in order to persist data (for at least two different entities / classes) in your app (2 points)
+
+- you should be able to insert, read, update and delete data from the database (CRUD)
+
+Implement a UserControl in a separate project (so that it can be easily used in several projects and distributed to other developers) and use it in your app. (0.50 points)
+
+
+- the UserControl should provide a useful functionality for your app (ex: don't implement a control showing the current time).  
+- please don't copy&paste a clock usercontrol from the internet ;)
+
+Data binding (1.25 points)
+
+- 0.25p: Data binding for individual fields  
+- 1.00p: Implement the ViewModel (Controller) pattern
+
+BONUS POINTS (0.50 EACH) 
+
+Use an Entity Framework code-first approach
+
+Offer the possibility to print a document (with PrintPreview)
+
+Implement the drag & drop functionality or Clipboard (0.25 points)
+
+
